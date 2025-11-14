1074386844726df62c82c727ea703538cceca8f4
MkrtchyanKarina <mkrtchyan.karina.06@mail.ru>
2025-11-14 15:20:05 +0300

    Исправлены тесты

--- Courses/CoursesTests/UnitTest1.cs
+++ Courses/CoursesTests/UnitTest1.cs
@@ -5,7 +5,24 @@ namespace CoursesTests
         [Fact]
         public void Test1()
         {
+            
+            var stringWriter = new StringWriter();
+            var originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
 
+            // Код, который пишет в консоль
+            Console.WriteLine("Hello, World!");
+            var result = stringWriter.ToString();
+
+            // Assert
+            Assert.Contains("Hello, World!", result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+            Assert.Equal(1, 1);
         }
     }
 }
